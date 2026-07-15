353fa7f991f718afd27b70dd4b93101b637f50d8
xiaowei <3442359407@qq.com>
2026-07-15 09:46:01 +0800

    Add node_modules to .gitignore

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/data/data/com.termux/files/home/FCL/FCL-Beginner-s-Guide/node_modules/infima/dist/css/default/default.css"),
-  require("/data/data/com.termux/files/home/FCL/FCL-Beginner-s-Guide/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/data/data/com.termux/files/home/FCL/FCL-Beginner-s-Guide/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/data/data/com.termux/files/home/FCL/FCL-Beginner-s-Guide/src/css/custom.css"),
-];
